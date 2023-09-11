c266f61be450739db4804169af12580e4a587a93
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-09-11 12:34:00 +0800

    implement header show/hide animation depending on the scroll direction

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,6 +33,33 @@ const socials = [
 ];
 
 const Header = () => {
+  //Step 5 - header show/hide animation depending on the scroll direction
+  const headerRef = useRef(null);
+
+    useEffect(() => {
+      let prevScrollPos = window.scrollY;
+
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      const shouldShowHeader = prevScrollPos > currentScrollPos;
+      updateHeaderTransform(shouldShowHeader);
+
+      prevScrollPos = currentScrollPos;
+    };
+
+    const updateHeaderTransform = (shouldShow) => {
+      const headerElement = headerRef.current;
+      if (!headerElement) return;
+
+      headerElement.style.transform = shouldShow ? "translateY(0)" : "translateY(-200px)";
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    
+    return () => window.removeEventListener('scroll', handleScroll);
+
+  }, []);
+
   const handleClick = (anchor) => () => {
     const id = `${anchor}-section`;
     const element = document.getElementById(id);
@@ -43,6 +70,7 @@ const Header = () => {
       });
     }
   };
+
 
   return (
     <Box
@@ -55,6 +83,7 @@ const Header = () => {
       transitionDuration=".3s"
       transitionTimingFunction="ease-in-out"
       backgroundColor="#18181b"
+      ref={headerRef} 
     >
       <Box color="white" maxWidth="1280px" margin="0 auto">
         <HStack
